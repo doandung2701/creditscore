4d6d56b9c3991fb5990b5ff3b319d17cd17ab589
doandung2701 <doandung2701@gmail.com>
2020-11-24 22:37:20 +0700

    final code

--- src/main/java/com/devc/creditscore/CreditscoreApplication.java
+++ src/main/java/com/devc/creditscore/CreditscoreApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
 
-@SpringBootApplication(exclude={BatchAutoConfiguration.class})
+@SpringBootApplication
 public class CreditscoreApplication {
 
 	public static void main(String[] args) {
